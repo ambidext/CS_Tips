7dc6f3e1fa4813b3089def21a013eb70d8152834
ambidext-PC\ambidext <ambidext@gmail.com>
2019-03-31 11:35:30 +0900

    Add OpenCV Projects

---
+++ OpenCVSharp_Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCVSharp_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoCapture capture = new VideoCapture(@"e:\Temp\test.mp4");
+
+            int sleepTime = (int)Math.Round(1000 / capture.Fps);
+
+            using (Window window = new Window("capture"))
+            using (Mat image = new Mat()) // Frame image buffer
+            {
+                // When the movie playback reaches end, Mat.data becomes NULL.
+                while (true)
+                {
+                    capture.Read(image); // same as cvQueryFrame
+                    if (image.Empty())
+                        break;
+
+                    window.ShowImage(image);
+                    Cv2.WaitKey(sleepTime);
+                }
+            }
+        }
+    }
+}
---
+++ OpenCVSharp_Winform/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using OpenCvSharp.Extensions;
+
+namespace OpenCVSharp_Winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //  base.OnPaintBackground(e);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //base.OnPaint(e);
+            //var g = e.Graphics;
+            //Bitmap item;
+            //while (true)
+            //{
+            //    if (_q.TryTake(out item) == true)
+            //    {
+            //        g.DrawImage(item, new PointF(0, 0));
+            //        item.Dispose();
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+        }
+
+        BlockingCollection<Bitmap> _q = new BlockingCollection<Bitmap>();
+
+        private Thread camera;
+
+        private void CaptureCameraCallback()
+        {
+            VideoCapture capture = new VideoCapture(@"e:\Temp\test.mp4");
+
+            if (capture.IsOpened() == false)
+            {
+                return;
+            }
+
+            int fps = (int)capture.Fps;
+
+            int expectedProcessTimePerFrame = 1000 / fps;
+            Stopwatch st = new Stopwatch();
+            st.Start();
+
+            using (Mat image = new Mat())
+            {
+                while (true)
+                {
+                    long started = st.ElapsedMilliseconds;
+                    capture.Read(image);
+
+                    if (image.Empty() == true)
+                    {
+                        break;
+                    }
+
+                    _q.Add(BitmapConverter.ToBitmap(image));
+                    //this.Invoke((Action)(() => this.Invalidate()));
+                    pictureBox1.Invoke((Action)(() => pictureBox1.Invalidate()));
+                    int elapsed = (int)(st.ElapsedMilliseconds - started);
+                    int delay = expectedProcessTimePerFrame - elapsed;
+
+                    if (delay > 0)
+                    {
+                        Thread.Sleep(delay);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            camera = new Thread(CaptureCameraCallback);
+            camera.IsBackground = true;
+            camera.Start();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            var g = e.Graphics;
+            Bitmap item;
+            while (true)
+            {
+                if (_q.TryTake(out item) == true)
+                {
+                    g.DrawImage(item, new PointF(0, 0));
+                    item.Dispose();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
